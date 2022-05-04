c02dde9052ce7a29c04d1a3773c9177b5379baa0
Vo Anh Hieu <voanhhieu10250@gmail.com>
2022-05-04 16:06:53 +0700

    Clean up layout

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the Rails application.
-require_relative "application"
+require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Remove .field_with_errors div which covers input field when there are some input errors
+ActionView::Base.field_error_proc = proc do |html_tag, _instance|
+  html_tag.html_safe
+end
