97a2a8d11b793e97fa13fce8eed3f9db9f29672a
Vo Anh Hieu <voanhhieu10250@gmail.com>
2022-05-10 18:13:18 +0700

    add user signup func

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   resources :articles # , only: %i[show index new create edit update]
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'user#create'
+  resources :users, except: %i[new]
 end
