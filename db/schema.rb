c769ac7a712b5bcab520956fe37eee8bd4f1f194
Vo Anh Hieu <voanhhieu10250@gmail.com>
2022-05-10 16:56:05 +0700

    add secure password for user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_08_081929) do
+ActiveRecord::Schema.define(version: 2022_05_10_094809) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_05_08_081929) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
