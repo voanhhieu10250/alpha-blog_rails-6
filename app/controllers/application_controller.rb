6c40fd732049b7251cd921c1fa2956c164ace88e
Vo Anh Hieu <voanhhieu10250@gmail.com>
2022-05-14 16:02:04 +0700

    Controller methods as helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
+  def logged_in?
+    # turn current_user to boolean and return it
+    !!current_user
+  end
 end
